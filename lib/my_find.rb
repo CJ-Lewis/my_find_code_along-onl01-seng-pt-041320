f42559f0777557f92d472d73c687f747aff1d7d8
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-22 23:48:53 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,11 @@ def my_find(collection)
   i = 0
   while i <
     collection.length
-    
-    yield(collection[i])
+    return
+    (collection[i]) if
+    return
+  collection[i]
+end
       i = i + 1
     end
 end
